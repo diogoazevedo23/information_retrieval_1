0ff9124c89e8c22d319255108409c80a1b7faea2
Diogo Miguel Azevedo <dma23@ua.pt>
2021-11-19 15:35:21 +0000

    First commit
    First commit

---
+++ index1.py
@@ -0,0 +1,103 @@
+"""
+
+    Autores:
+    Diogo Azevedo nº 104654 / Ricardo Madureira nº 104624
+    19/11/2021
+
+"""
+
+# Imports
+import sys
+import os
+import json  # Json functions
+import time  # Time functions
+
+
+""" Main Class """
+
+
+class Merger:
+
+    result = {}     # Final dictionary with the merged blocks
+    contagem = []   # Array with documents in which the term searched will appear
+
+    def __init__(self, path="blocks/"):
+        self.path = path
+
+    """ Merge the blocks into a final block """
+
+    def merge_docs(self):
+        # print("A ler ficheiros")
+        files = [f for f in os.listdir(
+            self.path) if f.endswith(".txt")]  # Get all blocks
+
+        for file in files:
+            full_path = self.path + file
+            d = json.load(open(full_path))
+            for word in d:
+                if word not in self.result:
+                    self.result[word] = dict()
+                for doc in d[word]:
+                    if doc not in self.result[word]:
+                        self.result[word][doc] = d[word][doc]
+                    else:
+                        self.result[word][doc] += d[word][doc]
+
+    """ Save the merged dictionary """
+
+    def save_merge(self):
+        with open('finalBlock/completeText.txt', 'w') as f:
+            json.dump(self.result, f)
+        print("\nSaved")
+
+    """ Search for a word """
+
+    def index_searcher(self, termo_final):
+        dictionary_final = {}
+
+        full_path2 = "finalBlock/completeText.txt"
+        dictionary_final = json.load(open(full_path2))
+        if termo_final in dictionary_final:
+            self.contagem.append(dictionary_final[termo_final].keys())
+
+        print("Contagem -> ", self.contagem)
+
+    """ Get size of nested dictionary """
+
+    def calculate_dict_size(self):
+        mem_size = 0
+        for key, value in self.result.items():
+            mem_size += sys.getsizeof(value)
+
+        return mem_size + sys.getsizeof(self.result)
+
+    """ Get nº of terms """
+
+    def print_results(self):
+        # print("\n\nresult ->", self.result.items())
+        return (len(self.result))
+
+
+""" Main """
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print("Usage: py teste1.py termo('diogo')"
+              + "\n** CHOICES **"
+              + "\ntermo = termo que pretende procurar no index")
+        sys.exit(1)
+
+    try2 = Merger()
+
+    indexSearchStart = time.time()
+    try2.index_searcher(sys.argv[1])
+    indexSearchEnd = time.time()
+    indexSearchFinal = indexSearchEnd - indexSearchStart
+
+    with open("finalResult/finalAnswers.txt", "a") as f:
+        print("e) Amount of time taken to start up an index searcher, after the final index is written to disk ==",
+              indexSearchFinal, "s.", file=f)
+        print("\nFicheiros em que o termo", sys.argv[1], "aparece =", len(
+            try2.contagem), "documentos", file=f)
+        # print("\nFicheiros em que o termo", sys.argv[1], "aparece =", try2.contagem, file=f)
---
+++ teste1.py
@@ -0,0 +1,187 @@
+"""
+
+    Autores:
+    Diogo Azevedo nº 104654 / Ricardo Madureira nº 104624
+    19/11/2021
+
+"""
+
+# Imports
+import sys
+import os
+from testeTokenizer import Tokenizer    # Import of Tokenizer
+from index1 import Merger               # Import of Merger
+import json                             # Json Functions
+import time                             # Time functions
+from collections import OrderedDict     # Order dictionaries
+import csv                              # Ler ficheiros csv
+import psutil                           # Check memory
+import threading                        # threads     // Not used yet
+
+
+""" Main class """
+
+
+class teste1:
+
+    PostingList = {}            # Dicionario das postings lists
+    Sorted_PostingList = {}     # Dicionario das postings lists ordenadas
+    d = {}                      # para testes
+    BlockFilesNumber = 0        # Numero de blocos temporarios criados
+    total_mem_size = 0          # Total de memoria usada no final da indexação
+    nTokens = 0
+
+    """ Initialize some functions/variables """
+
+    def __init__(self, min_tamanho, tokenizer_mode, chunksize, file='files/teste1.txt'):
+        self.tokenizer_mode = tokenizer_mode
+        self.file = file
+        self.chunksize = chunksize
+        self.tokenizer = Tokenizer(min_tamanho, tokenizer_mode)
+        self.merger = Merger()
+
+    """ Function to send chunks of data to processing """
+
+    def gen_chunks(self, reader):
+        chunk = []
+        for i, line in enumerate(reader):
+            if (i % self.chunksize == 0 and i > 0):
+                # print("\n Inside line -->", line)
+                # print("Inside Chunk -->", chunk)
+                yield chunk
+                del chunk[:]
+            chunk.append(line)
+        # print("\n Outside Line -->", line)
+        # print("Outside Chunk -->", chunk)
+        yield chunk
+
+    """ Processing of data with SPIMI implementation """
+
+    def SPIMIChunk(self):
+        num = 0
+        num1 = 0
+
+        with open(self.file, newline='', encoding="utf-8") as csvfile:
+            reader = csv.DictReader(
+                csvfile, delimiter="\t", quoting=csv.QUOTE_NONE)    # Reading csv/tsv files
+            """ If you want to switch from tsv to csv files, change the  delimiter="\t", to  delimiter="," """
+            print("Reading tsv")
+
+            for chunk in self.gen_chunks(reader):
+                tokens = []    # Words
+                print("\n\n\t\t** ITERATION Nº", num1, "**\n")
+                for row in chunk:
+                    index = row['review_id']
+                    appended_string = row['product_title'] + " " + \
+                        row['review_headline'] + " " + \
+                        row['review_body']                  # Join title, headline and body rows
+                    tokens += self.tokenizer.tokenize(
+                        appended_string, index)             # Apply tokenizer of the tokens
+
+                # print("\nFinal Tokens --> ", tokens)
+                print("\nIndexing block ", num)
+                num += 1
+                self.criarBlocos(tokens)
+                num1 += 1
+
+    """ Indexing of datachunks that were already processed """
+
+    def criarBlocos(self, tokens):
+        for token in tokens:
+            term = token[0]
+            docID = token[1]
+            if term not in self.PostingList:
+                self.PostingList[term] = {docID: 1}
+            else:
+                if docID not in self.PostingList[term]:
+                    self.PostingList[term].update({docID: 1})
+                else:
+                    self.PostingList[term][docID] += 1
+
+        # print("\nPostingList -->", self.PostingList)
+        self.Sorted_PostingList = sorted(self.PostingList)
+        # print("\nSorted_PostingList -->", self.Sorted_PostingList)
+
+        self.writeToBlock()
+
+    """ Sort the chunks that were already processed and indexed and write them to a block """
+
+    def writeToBlock(self):
+
+        print("\nWriting to block")
+
+        sorted_dictionary = OrderedDict()
+        with open('blocks/' + str(self.BlockFilesNumber) + '.txt', 'w') as f:
+            for i in self.Sorted_PostingList:
+                sorted_dictionary[i] = self.PostingList[i]
+            # print("\nsorted_dictionary -->", sorted_dictionary.items())
+            json.dump(sorted_dictionary, f)
+            self.BlockFilesNumber += 1
+
+        # print("\nPostingList -> ", self.PostingList)
+        # print("\nSorted_PostingList -> ", self.Sorted_PostingList)
+
+        self.PostingList.clear()
+        self.Sorted_PostingList.clear()
+        print("\nFinished writing block")
+
+    """ Merge and save the merged block """
+
+    def pos_index(self):
+        self.merger.merge_docs()
+        self.merger.save_merge()
+
+    """ Get size of final dictionary """
+
+    def pos_index2(self):
+        return self.merger.print_results()
+
+    """ Size of final dictionary(Gb) """
+
+    def sizeOfDictInGb(self):
+        return self.merger.calculate_dict_size()
+
+    """ Print final answers to a file """
+
+    def printToFile(self, messageA, messageB, messageC, messageD):
+        with open('finalResult/finalAnswers.txt', 'w') as f:
+            print("Ficheiro ==", self.file, file=f)
+            print("\n\t**Perguntas**\n\n", file=f)
+            print("a) Total indexing time ==", messageA, "s.", file=f)
+            messageB2 = (f"{messageB/float(1<<30):,.3f} GB")
+            print("b) Total index size on disk ==", messageB2, file=f)
+            print("c) Vocabulary size(number of terms) ==",
+                  messageC, "terms", file=f)
+            print("d) Number of temporary index segments written to disk (before merging) ==",
+                  messageD, "blocks", file=f)
+
+
+""" Main """
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 5:
+        print("Usage: py teste1.py min_tamanho_palavra(no/4) Stopwords('yes/no/filepath') stemmer('yes/no') chunksize(4)"
+              + "\n** CHOICES **"
+              + "\nmin_tamanho_palavra: Can be choosen with a number or desativacted with 'no'"
+              + "\nstopwords: 'yes', 'no' or pathfile to the file that u want to use"
+              + "\nstemmer: 'yes' or 'no'"
+              + "\nchunkzise: integer"
+              + "\ntermo: Term that you want to find in the corpus")
+        sys.exit(1)
+
+    try1 = teste1(
+        sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]))
+
+    totalIndexingTimeStart = time.time()
+    try1.SPIMIChunk()
+
+    print("\n\nStarging to Merge")
+    mergeStartTime = time.time()
+    try1.pos_index()
+    mergeEndTime = time.time()
+    print("\nFinished Merging ==", mergeEndTime - mergeStartTime, "segundos")
+    totalIndexingTimeEnd = time.time()
+    totalIndexingTimeFinal = totalIndexingTimeEnd - totalIndexingTimeStart
+    try1.printToFile(totalIndexingTimeFinal, try1.sizeOfDictInGb(),
+                     try1.pos_index2(), try1.BlockFilesNumber)
